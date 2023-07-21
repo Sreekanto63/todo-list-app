79b5d01354c7f2919643b9832b69bd444069e6c4
Sreekanto Biswas <sreekanto63@gmail.com>
2023-07-21 21:07:09 +0600

    React todo-list-app

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleTaskInputChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  const removeTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="App">
+      <h1>To-Do List</h1>
+      <div className="task-input">
+        <input
+          type="text"
+          value={newTask}
+          onChange={handleTaskInputChange}
+          placeholder="Enter a new task"
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => removeTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+
+
